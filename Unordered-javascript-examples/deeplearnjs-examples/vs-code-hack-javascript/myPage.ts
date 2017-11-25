cc2dcbd9dc1e9dc640d0254e1e0aa02670cb90bb
Jeremy Ellis <keyfreemusic@gmail.com>
2017-11-24 18:19:15 -0800

    Update myPage.ts

--- Unordered-javascript-examples/deeplearnjs-examples/vs-code-hack-javascript/myPage.ts
+++ Unordered-javascript-examples/deeplearnjs-examples/vs-code-hack-javascript/myPage.ts
@@ -1,24 +1,26 @@
 <script src="https://unpkg.com/deeplearn@0.3.11/dist/deeplearn.js"> </script> 
 <script>  dl = deeplearn </script>
 
-
 <script>
 
-import * as dl from 'deeplearn';
+ // To run as a webpage change extension from .ts to .html
+ // The comment out the import line below
+import * as dl from 'deeplearn';  
+
 async function runExample() {
     const math = new dl.NDArrayMathGPU();
     const a = dl.Array1D.new([1, 2, 3]);
     const b = dl.Scalar.new(2);
     const result = math.add(a, b);
-    
-     document.getElementById('myDiv01').innerHTML = await result.data() + 'aaa<br>'
+
+    document.getElementById('myDiv01').innerHTML = await result.data() + '<br>'
 }
 
 </script>
 
 <input type=button value="wow" onclick="{
-    runExample()
-  }">
+  runExample()
+}">
   
-  <div id="myDiv01">...</div>
+<div id="myDiv01">...</div>
   
