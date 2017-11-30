c34e0f0533f1e484bc9c3d6a4361ec5cff18a219
Jeremy Ellis <keyfreemusic@gmail.com>
2017-11-30 10:39:12 -0800

    Update tensors.js

--- Unordered-javascript-examples/deeplearnjs-examples/tensors/tensors.js
+++ Unordered-javascript-examples/deeplearnjs-examples/tensors/tensors.js
@@ -26,33 +26,27 @@ async function myTensorTable(myDiv, myOutTensor, myCols, myTitle){
 async function myTest01(){
   dl = deeplearn 
   const matrixShape = [2, 3];  // 2 rows, 3 columns.
-  const myWeights  = dl.Array2D.new([3, 4], [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]);
-  const myInputs = dl.Array2D.new([3, 4], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const myWeights  = dl.Array2D.new([3, 3], [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]);
+  const myInputs = dl.Array2D.new([3, 3], [1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const myBias    = dl.Scalar.new(2.3);
 
  //const myOutput = await my2D.data()
- document.getElementById('myDiv01').innerHTML = '<br>Printing myWeights [3, 4] [rows, columns] <br>'
- await myTensorTable('myDiv01', myWeights, 4, 'Weights')  
+ await myTensorTable('myDiv01', myWeights, 3, 'Weights [3x3]')  
 
 
- document.getElementById('myDiv01').innerHTML += 'Printing myInputs [3, 4] [rows, columns] <br>'
- await myTensorTable('myDiv01', myInputs, 4, 'Inputs')  
+ await myTensorTable('myDiv01', myInputs, 3, 'Inputs [3x3]')  
 
 
  const myBiasOutput = await myBias.data();
- document.getElementById('myDiv01').innerHTML += 'Printing myBias Scalar = ' + myBiasOutput + ' <br><hr>'
-
-
-
-
+ document.getElementById('myDiv01').innerHTML += 'Bias Scalar = ' + myBiasOutput + ' <br><hr>'
 
 
  //Generic math ability set
  var myAlgorithm = new dl.NDArrayMathGPU()
 
 
-
-
+  var  myProduct = myAlgorithm.matMul(myWeights, myInputs)
+  await myTensorTable('myDiv01', myProduct, 3, 'Product of Weights x Inputs')  
 
 
  const myMatrix1  = dl.Array2D.new([2, 2], [3, 2, 5, 4]);
@@ -62,63 +56,44 @@ async function myTest01(){
  await myTensorTable('myDiv01', myMatrix2, 2, 'Table B')  
 
 
- myAlgorithm.scope(function() {
+
   var  myProduct = myAlgorithm.matMul(myMatrix1, myMatrix2)
   document.getElementById('myDiv01').innerHTML += ' A * B Matmul 2D = ' + myProduct.getValues()  +'<br>'   
 
   var  myProduct = myAlgorithm.matMul(myMatrix1, myMatrix2, 'TRANSPOSED', 'REGULAR')
   document.getElementById('myDiv01').innerHTML += 'A^T * B Matmul 2D = ' + myProduct.getValues()  +'<br>'  
 
-
   var  myProduct = myAlgorithm.matMul(myMatrix2, myMatrix1)
   document.getElementById('myDiv01').innerHTML += 'B * A Matmul 2D = ' + myProduct.getValues()  +'<br>' 
-
-
 
   var  myProduct = myAlgorithm.matMul(myMatrix2, myMatrix1, 'REGULAR', 'TRANSPOSED')
   document.getElementById('myDiv01').innerHTML += ' B * A^T  Matmul 2D = ' + myProduct.getValues()  +'<br><hr>'   
 
 
-
-
-
-
-})
   
 
 
-
-
-
-
-
-
-
-
  const w = new dl.Array1D.new([0.7, -0.3])
-
  const i = new dl.Array1D.new([0.1, 0.8])
  const b = dl.Scalar.new(2.1)
 
+ await myTensorTable('myDiv01', w, 2, 'w as weights')  
+ await myTensorTable('myDiv01', i, 2, 'i as inputs ')  
 
- 
- await myTensorTable('myDiv01', w, 2, 'w')  
- await myTensorTable('myDiv01', i, 2, 'i')  
-
- document.getElementById('myDiv01').innerHTML += 'Bias= '+ b.getValues()  +'<br>'
+ document.getElementById('myDiv01').innerHTML += 'b as Bias= '+ b.getValues()  +'<br><Br>'
  
  myAlgorithm.scope(function() {
    var myDot = myAlgorithm.dotProduct(w, i)
    var mySum = myAlgorithm.add(myDot, b)
    var mySig = myAlgorithm.sigmoid(mySum)
-   document.getElementById('myDiv01').innerHTML += 'easy sigmoid = '+mySig.getValues()  +'<br>'
+   document.getElementById('myDiv01').innerHTML += 'sigmoid(add( dot(w, i)+ b) ) = '+mySig.getValues()  +'<br>'
  })
 
  myAlgorithm.scope(function() {
     var myDot = myAlgorithm.dotProduct(w, i)
     var mySum = myAlgorithm.add(myDot, b)
     var mySig = myAlgorithm.sinh(mySum)
-    document.getElementById('myDiv01').innerHTML += 'easy sinH = '+mySig.getValues()  +'<br>'
+    document.getElementById('myDiv01').innerHTML += 'sinH(add( dot(w, i)+ b) ) = '+mySig.getValues()  +'<br>'
  })
 
 
@@ -126,7 +101,7 @@ async function myTest01(){
     var myDot = myAlgorithm.dotProduct(w, i)
     var mySum = myAlgorithm.add(myDot, b)
     var mySig = myAlgorithm.relu(mySum)
-    document.getElementById('myDiv01').innerHTML += 'easy relu = '+mySig.getValues()  +'<br>...<br>'   // Float32Array([3, 4, 5])
+    document.getElementById('myDiv01').innerHTML += 'relu(add( dot(w, i)+ b) ) = '+mySig.getValues()  +'<br><hr>'
  })
     
 
