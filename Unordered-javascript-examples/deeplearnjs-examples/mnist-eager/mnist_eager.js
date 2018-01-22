cb81ba2134405e4731a8dd4d229304bc5c35aca5
Jeremy Ellis <keyfreemusic@gmail.com>
2018-01-21 23:44:44 -0800

    Update mnist_eager.js

--- Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/mnist_eager.js
+++ Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/mnist_eager.js
@@ -1,43 +1,38 @@
-//import {MnistData} from './data';
-//import * as model from './model';
-//import * as ui from './ui';
-
-//data_new
 
 
-var data;
+
+let data;
+
+
 async function myLoad() {
-  data = new MnistData();      //from data_new.js
- // console.log(data)
-  await data.load();
+    document.getElementById('status').innerText += ' Loading...'
+    data = new MnistData();
+    await data.load();
 }
+
 
 async function myTrain() {
-  
-   console.log('8')
-  //ui.isTraining();                          //from ui.js
-  //trainingLog('fred')     //note: isTraining and Training log have async issues
-  document.getElementById('status').innerText = 'Training....'
-  console.log('9')
-  //await train(data, trainingLog);    //from model.js
-  await train(data);    //from model.js
-  
-   console.log('10')
+    document.getElementById('status').innerText += ' Training...'
+    await train(data);
 }
+
+
 
 async function myTest() {
-  const testExamples = 50;
-  const batch = data.nextTestBatch(testExamples);
-  const predictions = predict(batch.xs);            // from model.js
-  const labels = classesFromLabel(batch.labels);   // from model.js
-
-  showTestResults(batch, predictions, labels);    //from ui.js
+    //document.getElementById('status').innerText += ' Testing...'
+    const testExamples = 50;
+    const batch = data.nextTestBatch(testExamples);
+    const predictions = predict(batch.xs);
+    const labels = classesFromLabel(batch.labels);
+    showTestResults(batch, predictions, labels);
 }
+
 
 async function myMnist() {
-  await myLoad();
-  await myTrain();
-  myTest();
+    await myLoad();
+    await myTrain();
+    myTest();
 }
 
-myMnist();
+
+
