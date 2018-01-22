531c6fe9baaea438a9a822737c54e1d6b615d1b5
Jeremy Ellis <keyfreemusic@gmail.com>
2018-01-21 22:23:10 -0800

    Update model.js

--- Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/model.js
+++ Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/model.js
@@ -10,35 +10,37 @@ const TRAIN_STEPS = 100;
 const IMAGE_SIZE = 784;
 const LABELS_SIZE = 10;
 
-//const math = dl.ENV.math;
+const mathModel2 = dl.ENV.math;
 
 const optimizer = new dl.SGDOptimizer(LEARNING_RATE);
 
 // Set up the model and loss function.
 
-var weights = dl.variable(dl.Array2D.randNormal([IMAGE_SIZE, LABELS_SIZE], 0, 1 / Math.sqrt(IMAGE_SIZE), 'float32'));
+const weights = dl.variable(dl.Array2D.randNormal([IMAGE_SIZE, LABELS_SIZE], 0, 1 / Math.sqrt(IMAGE_SIZE), 'float32'));
 
-var model = function (xs) {
-    return math.matMul(xs, weights);
-};
-
-var loss = function (labels, ys) {
-    return math.mean(math.softmaxCrossEntropyWithLogits(labels, ys));
-};
 
 
 
 // Train the model.
-async function train(data, log) {
+const model = function (xs) {
+  return mathModel2.matMul(xs, weights);
+};
+
+const loss = function (labels, ys) {
+  return mathModel2.mean(math.softmaxCrossEntropyWithLogits(labels, ys));
+};
+
+// Train the model.
+async function train(data)  {
   const returnCost = true;
   for (let i = 0; i < TRAIN_STEPS; i++) {
-    const cost = dl.optimizer.minimize(  () => {
+    const cost = optimizer.minimize( function () {
       const batch = data.nextTrainBatch(BATCH_SIZE);
 
       return loss(batch.labels, model(batch.xs));
     }, returnCost);
 
-    log("loss[" + i + "]: " + cost.dataSync());
+    console.log(`loss[${i}]: ${cost.dataSync()}`);
 
     await dl.util.nextFrame();
   }
@@ -54,9 +56,9 @@ async function test(data) {}
 
 // Predict the digit number from a batch of input images.
 function predict(x) {
-  const pred = math.scope(() => {
+  const pred = mathModel2.scope(() => {
     const axis = 1;
-    return math.argMax(model(x), axis);
+    return mathModel2.argMax(model(x), axis);
   });
   return Array.from(pred.dataSync());
 }
@@ -66,7 +68,7 @@ function predict(x) {
 // Given a logits or label vector, return the class indices.
 function classesFromLabel(y){
   const axis = 1;
-  const pred = math.argMax(y, axis);
+  const pred = mathModel2.argMax(y, axis);
 
   return Array.from(pred.dataSync());
 }
