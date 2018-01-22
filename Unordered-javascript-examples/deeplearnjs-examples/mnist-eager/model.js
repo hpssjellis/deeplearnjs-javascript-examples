442e1c697e7647c51420fa11d5203a651718fee6
Jeremy Ellis <keyfreemusic@gmail.com>
2018-01-21 23:45:06 -0800

    Update model.js

--- Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/model.js
+++ Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/model.js
@@ -1,74 +1,78 @@
 //import * as dl from 'deeplearn';
-//import {MnistData} from './data';
+
 
 // Hyperparameters.
 const LEARNING_RATE = .05;
 const BATCH_SIZE = 64;
 const TRAIN_STEPS = 100;
-
 // Data constants.
 const IMAGE_SIZE = 784;
 const LABELS_SIZE = 10;
 
-const mathModel2 = dl.ENV.math;
+
+//const math = dl.ENV.math;
+
 
 const optimizer = new dl.SGDOptimizer(LEARNING_RATE);
 
-// Set up the model and loss function.
 
+
+// Set up the model and loss function.
 const weights = dl.variable(dl.Array2D.randNormal([IMAGE_SIZE, LABELS_SIZE], 0, 1 / Math.sqrt(IMAGE_SIZE), 'float32'));
 
 
+const model = (xs) => {
+    return math.matMul(xs, weights);
+};
+
+
+
+const loss = (labels, ys) => {
+    return math.mean(math.softmaxCrossEntropyWithLogits(labels, ys));
+};
+
 
 
 // Train the model.
-const model = function (xs) {
-  return mathModel2.matMul(xs, weights);
-};
-
-const loss = function (labels, ys) {
-  return mathModel2.mean(math.softmaxCrossEntropyWithLogits(labels, ys));
-};
-
-// Train the model.
-async function train(data)  {
-  const returnCost = true;
-  for (let i = 0; i < TRAIN_STEPS; i++) {
-    const cost = optimizer.minimize( function () {
-      const batch = data.nextTrainBatch(BATCH_SIZE);
-
-      return loss(batch.labels, model(batch.xs));
-    }, returnCost);
-
-    console.log(`loss[${i}]: ${cost.dataSync()}`);
-
-    await dl.util.nextFrame();
-  }
+async function train(data) {
+    const returnCost = true;
+    for (let i = 0; i < TRAIN_STEPS; i++) {
+        const cost = optimizer.minimize(() => {
+            const batch = data.nextTrainBatch(BATCH_SIZE);
+            return loss(batch.labels, model(batch.xs));
+        }, returnCost);
+        document.getElementById('message').innerHTML = `loss[${i}]: ${cost.dataSync()}`
+        await dl.util.nextFrame();
+    }
 }
 
 
 
-
 // Tests the model on a set
-async function test(data) {}
+async function test(data) { }
 
 
 
 // Predict the digit number from a batch of input images.
 function predict(x) {
-  const pred = mathModel2.scope(() => {
-    const axis = 1;
-    return mathModel2.argMax(model(x), axis);
-  });
-  return Array.from(pred.dataSync());
+    const pred = math.scope(() => {
+        const axis = 1;
+        return math.argMax(model(x), axis);
+    });
+    return Array.from(pred.dataSync());
 }
+
 
 
 
 // Given a logits or label vector, return the class indices.
-function classesFromLabel(y){
-  const axis = 1;
-  const pred = mathModel2.argMax(y, axis);
-
-  return Array.from(pred.dataSync());
+function classesFromLabel(y) {
+    const axis = 1;
+    const pred = math.argMax(y, axis);
+    return Array.from(pred.dataSync());
 }
+
+
+
+
+
