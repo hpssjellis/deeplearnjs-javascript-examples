93f675167d12b152729cd5eb7f5efe2a10140851
Jeremy Ellis <keyfreemusic@gmail.com>
2018-01-21 22:23:56 -0800

    Update data_new.js

--- Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/data_new.js
+++ Unordered-javascript-examples/deeplearnjs-examples/mnist-eager/data_new.js
@@ -1,6 +1,6 @@
 //import * as dl from 'deeplearn';
 
-//const math = dl.ENV.math;
+const mathData = dl.ENV.math;
 
 const TRAIN_TEST_RATIO = 5 / 6;
 
@@ -33,15 +33,16 @@ class MnistData {
 
 
   async nextTrainBatch(batchSize) {
+    
+          console.log('nexttrainbatch start')
         var _this = this;
         return this.nextBatch(batchSize, this.trainingData, function () {
-            _this.shuffledTrainIndex =
-                (_this.shuffledTrainIndex + 1) % _this.trainIndices.length;
+            _this.shuffledTrainIndex = (_this.shuffledTrainIndex + 1) % _this.trainIndices.length;
             return _this.trainIndices[_this.shuffledTrainIndex];
         });
   }
 
-  async nextTestBatch(batchSize) {
+    async  nextTestBatch(batchSize) {
         var _this = this;
         return this.nextBatch(batchSize, this.testData, function () {
             _this.shuffledTestIndex =
@@ -50,7 +51,7 @@ class MnistData {
         });
   }
 
-  async nextBatch(batchSize, data, index) {
+    async  nextBatch(batchSize, data, index) {
         var xs = null;
         var labels = null;
         for (var i = 0; i < batchSize; i++) {
@@ -60,45 +61,48 @@ class MnistData {
             var label = data[1][idx].reshape([1, 10]);
             labels = concatWithNulls(labels, label);
         }
-        return { xs, labels};
+        return { xs: xs, labels: labels };
   }
 
-  async load() {
+    async load() {
     this.dataset = new dl.XhrDataset(mnistConfig);
-    console.log('1')
+  //  console.log(this.dataset);
     await this.dataset.fetchData();
     console.log('2')
-    this.dataset.normalizeWithinBounds(0, -1, 1);
+    //console.log(this.dataset);
+    await this.dataset.normalizeWithinBounds(0, -1, 1);
     console.log('3')
-    this.trainingData = this.getTrainingData();
-    console.log('this.trainingData[0].length'+this.trainingData[1].length)
-    this.testData = this.getTestData();
+    this.trainingData = await this.getTrainingData();
+        //console.log(this.trainingData);
+    console.log('4')
+    this.testData = await this.getTestData();
     console.log('5')
 
-    this.trainIndices = dl.util.createShuffledIndices(this.trainingData[0].length);
+    this.trainIndices = await dl.util.createShuffledIndices(this.trainingData[0].length);
      console.log('6')
-    this.testIndices = dl.util.createShuffledIndices(this.testData[0].length);
+    this.testIndices = await dl.util.createShuffledIndices(this.testData[0].length);
       console.log('7')
     
   }
 
-  async getTrainingData() {
+    async getTrainingData() {
         console.log('3A')
-    const [images, labels] = this.dataset.getData();
-    
+         const [images, labels] = await this.dataset.getData();
         console.log('3B')
-    const end = Math.floor(TRAIN_TEST_RATIO * images.length);
-    
+
+        console.log(images.length)
+        console.log(TRAIN_TEST_RATIO)
+        const end = Math.floor(TRAIN_TEST_RATIO * images.length);
         console.log('3C')
-    return [images.slice(0, end), labels.slice(0, end)];
+        return [images.slice(0, end), labels.slice(0, end)];
   }
 
-  async getTestData(){
+    async getTestData(){
     const data = this.dataset.getData();
     if (data == null) {
       return null;
     }
-    const [images, labels] = this.dataset.getData();
+    const [images, labels] = await this.dataset.getData();
     const start = Math.floor(TRAIN_TEST_RATIO * images.length);
     return [images.slice(start), labels.slice(start)];
   }
@@ -119,5 +123,5 @@ function concatWithNulls(ndarray1,ndarray2){
   } else if (ndarray2 === null) {
     return ndarray1;
   }
-  return math.concat2D(ndarray1, ndarray2, 0);
+  return mathData.concat2D(ndarray1, ndarray2, 0);
 }
